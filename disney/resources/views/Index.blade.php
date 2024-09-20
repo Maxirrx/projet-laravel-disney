42cbc35265887edd80160d83d746b5e350a4d498
Maxirrx <150622875+Maxirrx@users.noreply.github.com>
2024-09-20 10:03:12 +0200

    update eco

--- disney/resources/views/Index.blade.php
+++ disney/resources/views/Index.blade.php
@@ -5,9 +5,48 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Style.css">
     <title>Liste des Animaux</title>
 </head>
 <body>
+<div name="titre_tableau">
+    <h1>Tableau des Animaux</h1>
+</div>
+<table class="tableauanimaux" ">
+    <tr>
+        <th>ID</th>
+        <th>Nom</th>
+        <th>Prix</th>
+        <th>Description</th>
+        <th>Date de Naissance</th>
+        <th>Lien</th>
+    </tr>
+    <tr>
+        <td>f</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+    </tr>
+    <tr>
+        <td>f</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+    </tr>
+    <tr>
+        <td>f</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+        <td>5</td>
+    </tr>
+</table>
+
 
 </body>
 </html>
